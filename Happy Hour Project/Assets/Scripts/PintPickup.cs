8390a68f8acf5b822ef57fdc75cdf5e996c2126c
benk23 <n00221365@iadt.ie>
2025-02-24 16:48:53 +0000

    Prototype Pouring Drinks Complete

---
+++ Happy Hour Project/Assets/Scripts/PintPickup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PintPickup : MonoBehaviour
+{
+
+public GameObject PintOnPlayer;
+    void Start()
+    {
+        PintOnPlayer.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                
+                PintOnPlayer.SetActive(true);
+            }
+        }
+    }
+
+
+}

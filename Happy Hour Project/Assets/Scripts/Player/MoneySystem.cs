3928a3b1fc4b92515b503d5452409cb4aa860eba
n00221602 <125364606+n00221602@users.noreply.github.com>
2025-03-18 18:48:15 +0000

    Reapply "made changes again"
    This reverts commit ebce9c19bfeef4bfc2a7e37d4a62f118754c9fc4.

--- Happy Hour Project/Assets/Scripts/Player/MoneySystem.cs
+++ Happy Hour Project/Assets/Scripts/Player/MoneySystem.cs
@@ -6,66 +6,49 @@ using TMPro;
 public class MoneySystem : MonoBehaviour
 {
     private CustomerNPC customerNPC;
-    public float moneyBalance = 0f;
+    public float moneyBalance;
     public bool moneyGiven = false;
     [SerializeField] TextMeshProUGUI moneyBalanceText;
 
     void Start()
     {
-        moneyBalance = 0f;
-
-        customerNPC = FindObjectOfType<CustomerNPC>();
-        if (customerNPC == null)
-        {
-            Debug.LogError("CustomerNPC component not found on " + gameObject.name);
-        }
+        moneyBalanceText = GameObject.Find("Player/PlayerUi/MoneyBalance").GetComponent<TextMeshProUGUI>();
     }
 
     void Update()
     {
         Debug.Log(moneyBalance);
-        moneyBalanceText.text = moneyBalance.ToString();
 
         if (customerNPC == null)
         {
-            return;
+            customerNPC = FindObjectOfType<CustomerNPC>();
+            if (customerNPC == null)
+            {
+                Debug.LogError("CustomerNPC component not found.");
+                return;
+            }
         }
 
         // BEER MONEY
-        if (customerNPC.CustomerBeer != null && customerNPC.CustomerBeer.activeSelf)
+        if (customerNPC.CustomerBeer.activeSelf)
         {
-            if (!moneyGiven)
-            {
-                beerMoneyAddition();
-            }
-        }
-        else
-        {
-            moneyGiven = false;
+            beerMoneyAddition();
         }
 
         // RED WINE MONEY
-        if (customerNPC.CustomerRedWine != null && customerNPC.CustomerRedWine.activeSelf)
+        if (customerNPC.CustomerRedWine.activeSelf)
         {
-            if (!moneyGiven)
-            {
-                redWineMoneyAddition();
-            }
-        }
-        else
-        {
-            moneyGiven = false;
+            redWineMoneyAddition();
         }
 
         // WHITE WINE MONEY
-        if (customerNPC.CustomerWhiteWine != null && customerNPC.CustomerWhiteWine.activeSelf)
+        if (customerNPC.CustomerWhiteWine.activeSelf)
         {
-            if (!moneyGiven)
-            {
-                whiteWineMoneyAddition();
-            }
+            whiteWineMoneyAddition();
         }
-        else
+
+        // Reset moneyGiven only when all customers are inactive
+        if (!customerNPC.CustomerBeer.activeSelf && !customerNPC.CustomerRedWine.activeSelf && !customerNPC.CustomerWhiteWine.activeSelf)
         {
             moneyGiven = false;
         }
@@ -73,25 +56,43 @@ public class MoneySystem : MonoBehaviour
 
     void beerMoneyAddition()
     {
-        Debug.Log("CustomerBeer is active.");
-        moneyBalance += 5f;
-        Debug.Log("Your Total Balance Is: " + moneyBalance);
-        moneyGiven = true;
+        if (!moneyGiven)
+        {
+            Debug.Log("CustomerBeer is active.");
+            moneyBalance += 5f;
+            Debug.Log("Your Total Balance Is: " + moneyBalance);
+            moneyGiven = true;
+            UpdateText();
+        }
     }
 
     void redWineMoneyAddition()
     {
-        Debug.Log("CustomerRedWine is active.");
-        moneyBalance += 8f;
-        Debug.Log("Your Total Balance Is: " + moneyBalance);
-        moneyGiven = true;
+        if (!moneyGiven)
+        {
+            Debug.Log("CustomerRedWine is active.");
+            moneyBalance += 8f;
+            Debug.Log("Your Total Balance Is: " + moneyBalance);
+            moneyGiven = true;
+            UpdateText();
+        }
     }
 
     void whiteWineMoneyAddition()
     {
-        Debug.Log("CustomerWhiteWine is active.");
-        moneyBalance += 8f;
-        Debug.Log("Your Total Balance Is: " + moneyBalance);
-        moneyGiven = true;
+        if (!moneyGiven)
+        {
+            Debug.Log("CustomerWhiteWine is active.");
+            moneyBalance += 8f;
+            Debug.Log("Your Total Balance Is: " + moneyBalance);
+            moneyGiven = true;
+            UpdateText();
+        }
+    }
+
+    void UpdateText()
+    {
+        moneyBalanceText.text = "$" + moneyBalance;
     }
 }
+
